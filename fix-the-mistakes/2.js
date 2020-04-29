d834939a39c40af7882575b474500d426698bd2e
Francisco Fernández <ferrycosv@gmail.com>
2020-04-29 15:43:02 +0200

    Completed

--- fix-the-mistakes/2.js
+++ fix-the-mistakes/2.js
@@ -1,5 +1,5 @@
 try {
-  const title = 'mistakes-2';
+  const title = "mistakes-2";
   console.group(title);
   // there is a comment by each method saying how many bugs are in it
   // write a little note about each after you fix it for later study
@@ -8,98 +8,117 @@ try {
     state: {
       number: 3,
       modulo: 2,
-      result: 1
+      result: 1,
     },
     log: [],
-    updateState: function (num, mod) { // 5 mistakes
-      this.number = num;
-      this.modulo = mod;
-      this.result = this.num % this.mod;
+    updateState: function (num, mod) {
+      // 5 mistakes
+      this.state.number = num;
+      this.state.modulo = mod;
+      this.state.result = num % mod;
     },
-    handler: function (event) { // 2 mistakes
+    handler: function (event) {
+      // 2 mistakes
       // debugger;
-      const form = event.form;
+      const form = event.target.form;
       const newNum = Number(form.newNum.value);
       const newMod = Number(form.newMod.value);
       this.updateState(newNum, newMod);
       form.lastChild.value = this.state.result;
-      this.log.push(
-        JSON.parse(JSON.stringify(this.state))
-      );
+      this.log.push(JSON.parse(JSON.stringify(this.state)));
     },
-    view: function (id) { // 2 mistakes
+    view: function (id) {
+      // 2 mistakes
       // debugger;
-      const outputEl = document.createElement('code');
-      outputEl.marginLeft = '5%';
+      const outputEl = document.createElement("code");
+      outputEl.marginLeft = "5%";
       outputEl.innerHTML = this.state.result;
 
-      const numInputEl = document.createElement('input');
-      numInputEl.type = 'number';
-      numInputEl.name = 'newNum';
+      const numInputEl = document.createElement("input");
+      numInputEl.type = "number";
+      numInputEl.name = "newNum";
       numInputEl.value = this.state.number;
       numInputEl.onclick = this.handler.bind(this);
 
-      const modInputEl = document.createElement('input');
-      modInputEl.type = 'number';
-      modInputEl.name = 'newMod';
+      const modInputEl = document.createElement("input");
+      modInputEl.type = "number";
+      modInputEl.name = "newMod";
       modInputEl.value = this.state.modulo;
       modInputEl.onclick = this.handler.bind(this);
 
-      const formEl = document.createElement('form');
+      const formEl = document.createElement("form");
       formEl.appendChild(numInputEl);
-      formEl.appendChild(document.createTextNode(' % '));
+      formEl.appendChild(document.createTextNode(" % "));
       formEl.appendChild(modInputEl);
-      formEl.appendChild(document.createTextNode(' = '));
+      formEl.appendChild(document.createTextNode(" = "));
       formEl.appendChild(outputEl);
 
-      const container = document.createElement('div');
+      const container = document.createElement("div");
       container.id = id;
-      container.className = 'exercise';
-      container.onclick = (function (e) {
+      container.className = "exercise";
+      container.onclick = function (e) {
         if (e.target === e.currentTarget) console.log(title, this);
-      }).bind(this);
+      }.bind(this);
 
       return container;
     },
-  }
+  };
 
-  document
-    .getElementById('root')
-    .appendChild(obj.view(title));
-
+  document.getElementById("root").appendChild(obj.view(title));
 
   // there are no mistakes in the tests
   const assert = (assertion, message) => {
     if (assertion) {
-      console.log('%cPASS: ' + message, 'color:green');
+      console.log("%cPASS: " + message, "color:green");
     } else {
-      console.log('%cFAIL: ' + message, 'color:red');
+      console.log("%cFAIL: " + message, "color:red");
     }
   };
 
-  assert(JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}', 'Test 0');
+  assert(
+    JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}',
+    "Test 0"
+  );
 
   obj.updateState(3, 2);
-  assert(JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}', 'Test 1');
+  assert(
+    JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}',
+    "Test 1"
+  );
 
   obj.updateState(2, 3);
-  assert(JSON.stringify(obj.state) === '{"number":2,"modulo":3,"result":2}', 'Test 2');
+  assert(
+    JSON.stringify(obj.state) === '{"number":2,"modulo":3,"result":2}',
+    "Test 2"
+  );
 
   obj.updateState(-1, 1);
-  assert(JSON.stringify(obj.state) === '{"number":-1,"modulo":1,"result":0}', 'Test 3');
+  assert(
+    JSON.stringify(obj.state) === '{"number":-1,"modulo":1,"result":0}',
+    "Test 3"
+  );
 
   obj.updateState(12, 4);
-  assert(JSON.stringify(obj.state) === '{"number":12,"modulo":4,"result":0}', 'Test 4');
+  assert(
+    JSON.stringify(obj.state) === '{"number":12,"modulo":4,"result":0}',
+    "Test 4"
+  );
 
   obj.updateState(8, -5);
-  assert(JSON.stringify(obj.state) === '{"number":8,"modulo":-5,"result":3}', 'Test 5');
+  assert(
+    JSON.stringify(obj.state) === '{"number":8,"modulo":-5,"result":3}',
+    "Test 5"
+  );
 
   obj.state = {
     number: 3,
     modulo: 2,
-    result: 1
+    result: 1,
   };
-  assert(JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}', 'Test 6');
+  assert(
+    JSON.stringify(obj.state) === '{"number":3,"modulo":2,"result":1}',
+    "Test 6"
+  );
 
   console.groupEnd();
 } catch (err) {
